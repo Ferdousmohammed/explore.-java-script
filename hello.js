18195a29a2e195ed33f36af6293ea71a0334af28
Ferdousmohammed <ferdousmohammed29@gmail.com>
2023-01-21 12:51:36 +0600

    first commit

---
+++ hello.js
@@ -0,0 +1,40 @@
+console.log(47);
+console.log('hey jack');
+console.log('my first love');
+console.log('lover boy Jhankar');
+console.log('we love js');
+
+var bananaPrice= 13;
+console.log('');
+var basVera=19;
+console.log(basVera);
+
+var exGf =10;
+console.log('exGf');
+
+var sarteGetsyoucloser=15;
+console.log('sarteGetsyoucloser 3456');
+
+var  seenAfter=21;
+console.log(typeof seenAfter);
+
+var myBabu=('19s iove');
+console.log(typeof myBabu);
+
+
+var sadiaIslam=(24);
+console.log(typeof sadiaIslam);
+
+var isHot=false;
+var montuisPilot=true;
+console.log(typeof isHot);
+console.log(typeof montuisPilot);
+
+var bananaPrice= 12;
+console.log(12);
+
+var bigBoss=56;
+console.log(56);
+
+var homoSepis= '21'
+console.log(typeof homoSepis );
